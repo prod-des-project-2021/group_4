d3cd9264fbb796cdf116d985b1fc7d777fb72241
Ville Prittinen <villepri@hotmail.com>
2021-11-08 10:48:38 +0200

    välilyönti = tuplavauhti
    display koko tulee nyt muuttujilla. lisäsin rajat ettei ikkunan ulkopuolelle pääse. välilyönti tuplaa vauhdin niin kauan kuin se on painettuna

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import pygame
 
 pygame.init()
-
-screen = pygame.display.set_mode((600,600))
+displaywidth = 900
+displayheight = 900
+screen = pygame.display.set_mode((displaywidth, displayheight))
 
 pygame.display.set_caption("Multiplayer Game")
 
@@ -28,14 +29,19 @@ while running:
     
     buttons = pygame.key.get_pressed()
 
-    if buttons[pygame.K_LEFT]:
+    if buttons[pygame.K_LEFT] and x > vel:
         x -= vel
-    if buttons[pygame.K_RIGHT]:
+    if buttons[pygame.K_RIGHT] and x < displaywidth - width + vel:
         x += vel
-    if buttons[pygame.K_UP]:
+    if buttons[pygame.K_UP] and y > vel:
         y -= vel
-    if buttons[pygame.K_DOWN]:
+    if buttons[pygame.K_DOWN] and y < displaywidth - height - vel:
         y += vel
+    if buttons[pygame.K_SPACE]:
+        vel = 4
+    else:
+        vel = 2
+
     player()
    
     
