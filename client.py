3219f6ffae376299aefa4887528db2e690d331a6
Krymyy <p-eetu@hotmail.com>
2021-11-10 14:03:52 +0200

    shooting
    shooting

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-import random
+
 
 from pygame.transform import rotate
 
@@ -12,8 +12,10 @@ displaywidth = 900
 displayheight = 900
 screen = pygame.display.set_mode((displaywidth, displayheight))
 
+#Caption
 pygame.display.set_caption("Multiplayer Game")
 
+#Player values
 playerImg = pygame.image.load('player.png')
 angle = 0
 x = 50
@@ -25,19 +27,34 @@ boostmodifier = 2
 boostspeed = basespeed * boostmodifier
 slowmodifier = 0.4
 vel = basespeed
+green = 0,255,0
+red = 255,0,0
+blue = 0,0,255
+yellow = 255,255,0
+white = 255,255,255
+black = 0,0,0
+
+#Bullet
+bulletImg = pygame.image.load('bullet1.png')
+bulletX = x
+bulletY = y
 
 
 
-def updateScreen():
-    screen.blit(playerImg, (x, y))
+
+def updateScreen(x2,y2):
+    screen.blit(playerImg, (x2, y2))
     pygame.display.update()
     
+def fire_bullet(x2,y2):
+    y2 -= 10
+    screen.blit(bulletImg,(x2 + 9 ,y2))
 
 running = True
 while running:
     #frame rate: 7 = about 142.9 fps, 17 = about 58.8 fps, 10 = exactly 100 fps
     pygame.time.delay(10)
-    screen.fill((255, 255, 255))
+    screen.fill((white))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -65,12 +82,21 @@ while running:
         y -= vel
     if buttons[pygame.K_s] and y < displaywidth - height - vel:
         y += vel
-
+    if buttons[pygame.K_x]:
+        if bulletY < 0:
+         bulletX = x
+         bulletY = y
+         fire_bullet(bulletX,bulletY)
+    if bulletY >= 0:
+        bulletY -= 5
+        fire_bullet(bulletX,bulletY)
     if buttons[pygame.K_SPACE]:
         vel = boostspeed
     else:
         vel = basespeed
-
-    updateScreen()
+    
+        
+    
+    updateScreen(x,y)
     
 pygame.quit()
