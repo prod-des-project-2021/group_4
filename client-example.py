627206bed42d86410b24d716cfde26f0d80db9dc
TheR0z0r <aus435@hotmail.com>
2021-11-18 15:25:31 +0200

    Added cool particles and some enemy mechanics

--- client-example.py
+++ client-example.py
@@ -1,20 +1,57 @@
 import pygame
 import math
 from pygame.math import Vector2
+import random
 
 ZERO_X = 1920/2
 ZERO_Y = 1080/2
+
+class ParticleEmitter:
+    def __init__(self, sprite):
+        self.sprite = sprite
+        self.particles = list()
+        self.x = 0
+        self.y = 0
+
+    def updatePosition(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, screen):
+        for particle in self.particles:
+            particle.lifetime = particle.lifetime - 1
+            if particle.lifetime == 0:
+                self.particles.remove(particle)
+            particle.draw(screen)
+
+    def addParticle(self, lifetime, velocity):
+        self.particles.append(Particle(self.sprite, self.x, self.y, lifetime, velocity))
+
+class Particle:
+    def __init__(self, sprite, position_x, position_y, lifetime, velocity):
+        self.sprite = sprite.copy()
+        self.dimensions = sprite.get_rect()
+        self.lifetime = lifetime
+        self.total_lifetime = lifetime
+        self.position = Vector2(position_x-self.dimensions.width/2, position_y-self.dimensions.height/2)
+        self.velocity = Vector2(velocity)
+        self.velocity.rotate_ip(random.randint(1,20))
+
+    def draw(self, screen):
+        self.position += self.velocity
+        self.sprite.set_alpha(self.lifetime/self.total_lifetime*255)
+        screen.blit(self.sprite, (int(self.position.x), int(self.position.y)))
 
 class Player:
     def __init__(self, sprite):
         self.sprite = sprite
 
         # rewritten movement with vectors
-        self.position = Vector2(150.0, 150.0)
+        self.position = Vector2(ZERO_X, ZERO_Y)
         self.UP = Vector2(0, 1)
         self.direction = Vector2(0, 1)
         self.velocity = Vector2(0, 0)
-        self.acceleration = 0.25
+        self.acceleration = 0.1
 
         self.angle = 0.0
         self.rotatedSprite = None
@@ -33,7 +70,6 @@ class Player:
     def rotate(self):
         self.direction = Vector2(self.UP)
         self.direction.rotate_ip(-self.angle)
-
         self.rotatedSprite = pygame.transform.rotate(self.sprite, int(self.angle)-90)
         self.dimensions = self.rotatedSprite.get_rect()
 
@@ -54,14 +90,48 @@ class Player:
         else:
             if(self.velocity.length() > 0):
                 # braking vector
-                braking = Vector2(0.15, 0.15)
+                braking = Vector2(0.05, 0.05)
                 rotation_angle = braking.angle_to(self.velocity)
                 braking.rotate_ip(rotation_angle)
                 self.velocity -= braking
 
+                # if velocity is smaller than braking, just zero it
+                if(self.velocity.length() < braking.length()):
+                    self.velocity = Vector2(0,0)
+
+
+
         self.position += self.velocity
         self.rotate()
 
+class Enemy:
+    def __init__(self, sprite):
+        self.sprite = sprite
+        self.dimensions = sprite.get_rect()
+        self.rotatedSprite = None
+        self.position = Vector2(random.randint(0, ZERO_X*2), random.randint(0, ZERO_Y*2))
+
+        self.UP = Vector2(0, -1)
+        self.angle = 0.0
+        self.direction = Vector2(0,0)
+        self.speed = 0.25
+
+        self.rotate()
+
+    def update(self, player_position):
+        self.angle = radToDec(math.atan2(self.position.x - player.position.x, self.position.y - player.position.y))
+        print(self.angle)
+        self.rotate()
+        self.position += self.direction * self.speed
+
+    def rotate(self):
+        self.direction = Vector2(self.UP)
+        self.direction.rotate_ip(-self.angle)
+        self.rotatedSprite = pygame.transform.rotate(self.sprite, int(self.angle)-90)
+        self.dimensions = self.rotatedSprite.get_rect()
+
+    def draw(self, screen):
+        screen.blit(self.rotatedSprite, (int(self.position.x-self.dimensions.width/2), int(self.position.y-self.dimensions.height/2)))
 
 def radToDec(rad):
     return (rad*180/math.pi)
@@ -78,12 +148,21 @@ if __name__ == '__main__':
 
     # resources
     playerImg = pygame.image.load('res/player.png')
+    enemyImg = pygame.image.load('res/enemy.png')
+    engineTrailImg = pygame.image.load('res/engine_trail_particle.png')
+
     player = Player(playerImg)
+    playerEngineTrail = ParticleEmitter(engineTrailImg)
+
+    enemies = list()
+    enemies.append(Enemy(enemyImg))
 
     # mouse boolean
     mouseDown = False
 
     while(running):
+        ZERO_X = pygame.display.Info().current_w / 2
+        ZERO_Y = pygame.display.Info().current_h / 2
 
         # get mouse position
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -92,9 +171,17 @@ if __name__ == '__main__':
 
         # draw stuff
         screen.fill(pygame.Color("black"))
+        playerEngineTrail.draw(screen)
         player.draw(screen)
 
+        for enemy in enemies:
+            enemy.update(player.position)
+            enemy.draw(screen)
+
         # update stuff
+        playerEngineTrail.updatePosition(player.position.x, player.position.y)
+        if(mouseDown):
+            playerEngineTrail.addParticle(15, -player.direction*5)
         player.update(mouseDown)
 
         for event in pygame.event.get():
@@ -105,5 +192,5 @@ if __name__ == '__main__':
             elif event.type == pygame.MOUSEBUTTONUP:
                 mouseDown = False
 
-        clock.tick(60)
+        clock.tick(120)
         pygame.display.flip()
