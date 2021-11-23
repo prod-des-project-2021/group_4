767a66e890557ae7e8014245d9aa4d53e4bb8295
TheR0z0r <aus435@hotmail.com>
2021-11-23 11:50:29 +0200

    Bugfix, now plain packets do not raise errors

--- networking/packet2.py
+++ networking/packet2.py
@@ -1,3 +1,5 @@
+import struct
+
 class Packet:
 
     #####################
@@ -5,7 +7,7 @@ class Packet:
     #####################
     def __init__(self):
         self.header     = "SP"
-        self.seq        = None
+        self.seq        = 0
         self.priority   = 0
         self.type       = 0
         self.payload    = b'0x00'
@@ -38,26 +40,36 @@ class Packet:
     # Decoding the packet from byteform #
     #####################################
     def decode(self, raw):
-        # fcalculating the size of the static part of the packet
-        staticSize = struct.calcsize('2s h i h i')
+        try:
+            # calculating the size of the static part of the packet
+            staticSize = struct.calcsize('2s h i h i')
 
-        # decoding the static part
-        decodedPacket = struct.unpack_from('2s h i h i', raw, 0)
+            # decoding the static part
+            decodedPacket = struct.unpack_from('2s h i h i', raw, 0)
 
-        self.header         = decodedPacket[0].decode()
-        self.priority       = decodedPacket[1]
-        self.seq            = decodedPacket[2]
-        self.type           = decodedPacket[3]
-        self.payloadLength  = decodedPacket[4]
+            self.header         = decodedPacket[0].decode()
+            self.priority       = decodedPacket[1]
+            self.seq            = decodedPacket[2]
+            self.type           = decodedPacket[3]
+            self.payloadLength  = decodedPacket[4]
 
-        # decoding the payload with a variable size
-        decodedPayload = struct.unpack_from(str(self.payloadLength)+'s', raw, staticSize)
-        self.payload = decodedPayload[0]
+            # decoding the payload with a variable size            
+            decodedPayload = struct.unpack_from(str(self.payloadLength)+'s', raw, staticSize)
+            self.payload = decodedPayload[0]
+
+            # when succesfully unpacked
+            # return true to to caller
+            return True
+        except struct.error:
+            # error is raised when struct can't be unpacked
+            # we just notify the caller that unpack failed -> unvalid packet
+            return False
 
     ###################
     # DEBUGGING PRINT #
     ###################
     def printDebug(self):
+        print("========== PACKET INFORMATION ===========")
         print("Header: "+str(self.header))
         print("Priority: "+str(self.priority))
         print("Sequence no.: "+str(self.seq))
