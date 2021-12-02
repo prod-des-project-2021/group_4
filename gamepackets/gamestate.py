d721dee1f0773dbf185fd9756d90039ffb934a4b
TheR0z0r <aus435@hotmail.com>
2021-12-02 15:54:21 +0200

    Added an explanation for messy one liner

--- gamepackets/gamestate.py
+++ gamepackets/gamestate.py
@@ -11,10 +11,15 @@ def gamestate_unpack(raw):
     front_padding = struct.calcsize(GAME_STATE_FORMAT)
 
     for i in range(0, player_count_flag[0]):
+        # so let me explain
+        # here we take the full raw packet, and cut
+        # a part that suits the PLAYER_STATE_FORMAT size, there is also a padding the size of GAME_STATE_FORMAT
+        # then we step the loop according to player_count_flag and cut the next part
+        # each cut part is passed to normal player state unpacker and there we get the players information 
         player = playerstate_unpack(raw[(front_padding+i*player_size):front_padding+player_size+i*player_size])
         players.append(player)
 
-    return players 
+    return players
 
 # players: list of players
 # returns list encoded in bytes
