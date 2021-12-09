3db8316a61183ec6545d17ae739f3e832b2a38fc
TheR0z0r <aus435@hotmail.com>
2021-12-09 14:56:54 +0200

    Gamestate now also includes players own id
    NOTE: gamestate unpack returns a tuple: player_id, playerlist!!

--- gamepackets/formats.py
+++ gamepackets/formats.py
@@ -3,4 +3,5 @@ PLAYER_STATE_FORMAT = "i d d d d d h h h"
 PLAYER_STATE = 11
 
 GAME_STATE = 51
-GAME_STATE_FORMAT = "i " # + n player states
+# | player own id | player count |
+GAME_STATE_FORMAT = "i i " # + n player states
--- gamepackets/gamestate.py
+++ gamepackets/gamestate.py
@@ -4,13 +4,16 @@ from .playerstate import *
 # raw: raw packet
 # returns a list of players
 def gamestate_unpack(raw):
-    player_count_flag = struct.unpack_from(GAME_STATE_FORMAT, raw, 0)
+    header_data = struct.unpack_from(GAME_STATE_FORMAT, raw, 0)
+    player_own_id = header_data[0]
+    player_count_flag = header_data[1]
+
     players = list()
 
     player_size = struct.calcsize(PLAYER_STATE_FORMAT)
     front_padding = struct.calcsize(GAME_STATE_FORMAT)
 
-    for i in range(0, player_count_flag[0]):
+    for i in range(0, player_count_flag):
         # so let me explain
         # here we take the full raw packet, and cut
         # a part that suits the PLAYER_STATE_FORMAT size, there is also a padding the size of GAME_STATE_FORMAT
@@ -19,11 +22,12 @@ def gamestate_unpack(raw):
         player = playerstate_unpack(raw[(front_padding+i*player_size):front_padding+player_size+i*player_size])
         players.append(player)
 
-    return players
+    return player_own_id, players
 
 # players: list of players
 # returns list encoded in bytes
-def gamestate_pack(players, me_id):
+def gamestate_pack(players, player_own_id):
+
     player_count = len(players)-1
 
     # getting a copy of gamestate format
@@ -34,11 +38,12 @@ def gamestate_pack(players, me_id):
 
     player_bytes = bytes()
     for player in players:
-        if player.id != me_id:
+        if player.id != player_own_id:
             player_packed = playerstate_pack(player)
             player_bytes = player_bytes + player_packed
 
     packed = struct.pack(gamestate_format,
+        player_own_id,
         player_count,
         player_bytes
     )
