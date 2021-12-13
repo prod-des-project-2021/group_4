290a4306fa2a05229501593314471662ed5f7a2b
TheR0z0r <aus435@hotmail.com>
2021-12-13 16:03:29 +0200

    Delete GameObjects directory

--- GameObjects/__init__.py
+++
@@ -1,6 +0,0 @@
-from .gameobject import GameObject
-from .textobject import TextObject
-from .player import Player
-from .bullet import Bullet
-from .explosion import Explosion
-from .enginetrail import ParticleEmitter, Particle
